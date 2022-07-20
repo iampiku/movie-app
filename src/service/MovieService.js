0e6bed8e8d9dd0621b7ede3a25262edda202df5a
iampiku <iampikuchatterjee@gmail.com>
2022-07-20 08:20:26 +0530

    store setup

--- src/service/MovieService.js
+++ src/service/MovieService.js
@@ -1,5 +1,40 @@
-import axio from 'axios';
+import axios from 'axios';
 
 export default class MovieService {
-	getPopularMovies() {}
+	async getPopularMovies() {
+		if ('caches' in window) {
+			caches.has('popular_movies').then((hasCache) => {
+				if (hasCache) {
+					caches.match('popular_movies').then((response) => {
+						return response.json();
+					});
+				} else {
+					caches.open('popular_movies').then(async (cache) => {
+						let response = await axios.get(
+							`${process.env.VUE_APP_API_URL}movie/popular?api_key=${process.env.VUE_APP_API_KEY}&language=en-US&page=1`
+						);
+						cache.put(
+							'POPULARMOVIES',
+							new Response(
+								JSON.stringify(response?.data?.results),
+								{
+									headers: {
+										'content-type': 'application/json',
+									},
+								}
+							)
+						);
+						return response?.data?.results;
+					});
+				}
+			});
+		}
+	}
+
+	async getLatestMovie() {
+		const response = await axios.get(
+			`${process.env.VUE_APP_API_URL}movie/latest?api_key=${process.env.VUE_APP_API_KEY}&language=en-US`
+		);
+		return response?.data?.results;
+	}
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,44 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+import MovieService from '@/service/MovieService';
+const movieService = new MovieService();
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+	state: {
+		popularMovies: [],
+		latestMovies: [],
+	},
+	getters: {
+		getPopularMovies(state) {
+			return state.popularMovies;
+		},
+
+		getLatestMovies(state) {
+			return state.latestMovies;
+		},
+	},
+	mutations: {
+		setPopularMovies(state, popularMovies) {
+			state.popularMovies = popularMovies;
+		},
+
+		setLatestMovies(state, latestMovies) {
+			state.latestMovies = latestMovies;
+		},
+	},
+	actions: {
+		async popularMovies({ commit }) {
+			const response = await movieService.getPopularMovies();
+			commit('setPopularMovies', response);
+		},
+
+		async latestMovies({ commit }) {
+			const response = await movieService.getLatestMovie();
+			commit('setLatestMovies', response);
+		},
+	},
+	modules: {},
+});
